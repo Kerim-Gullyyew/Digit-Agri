b2ae5d650d5742ce5eea4a9a426512fe6c428660
Kerim-Gullyyew <gullyyevk@gmail.com>
2023-12-04 15:53:27 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const App: React.FC = ({ }) => {
+  return (
+    <View style={{backgroundColor: 'white', flex: 1}}>
+      <Text>
+      </Text>
+    </View>
+  )
+}
+
+export default App
+const styles = StyleSheet.create({
+
+});
